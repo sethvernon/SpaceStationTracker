338aa7a414eaccdd553f99f9e6d59368fd268b74
Seth A. Vernon <seth.vernon@ichoiceappdesign.com>
2015-10-17 18:26:08 -0700

    custom annotation code

--- SpaceStationTracker/ViewController.swift
+++ SpaceStationTracker/ViewController.swift
@@ -5,14 +5,11 @@
 //  Created by Seth on 10/13/15.
 //  Copyright (c) 2015 Seth A. Vernon. All rights reserved.
 //
-
 import UIKit
 import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate
 {
-
-    
             @IBOutlet weak var mapView: MKMapView! {
                 didSet {
                     mapView.delegate = self
@@ -38,40 +35,66 @@ class ViewController: UIViewController, MKMapViewDelegate
             }
             lazy var spaceStationModel = SpaceStationModel()
             lazy var customAnnotation = CustomAnnotation()
+    
+            var newCoordinate = CLLocationCoordinate2DMake(Double(), Double())
             var spaceStationCoordinates = NSDictionary()
             var startCoordinate = CLLocationCoordinate2D()
             var activityIndicator = UIActivityIndicatorView()
-            var currentLatitude = Double?()
-            var currentLongitude = Double?()
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         activityIndicator.startAnimating()
+        
+        
         loadData()
+        
+
+        
+        setupAnnotation()
         
         
     }
+    
+    func setupAnnotation() {
+        
+        mapView.addAnnotation(customAnnotation)
+        mapView.setCenterCoordinate(customAnnotation.coordinate, animated: true)
+    }
+    
+    
+    
+    
     private func loadData() {
         
         SpaceStationModel.loadJSONDataWithCompletion { (locationDictionary) in
             
             self.spaceStationCoordinates = locationDictionary as NSDictionary
             
-            self.currentLatitude = self.spaceStationCoordinates.valueForKey("latitude")?.doubleValue
-            self.currentLongitude = self.spaceStationCoordinates.valueForKey("longitude")?.doubleValue
+            var currentLatitude = self.spaceStationCoordinates.valueForKey("latitude")!.doubleValue
+            var currentLongitude = self.spaceStationCoordinates.valueForKey("longitude")!.doubleValue
             
-            self.latitudeLabel.text = "\(self.currentLatitude)"
-            self.longitude.text = "\(self.currentLongitude)"
+            self.latitudeLabel.text = "\(currentLatitude)"
+            self.longitude.text = "\(currentLongitude)"
             
-            var newCoordinate = CLLocationCoordinate2DMake(self.currentLatitude!, self.currentLongitude!)
-            
-            self.customAnnotation.coordinate = newCoordinate
+            self.customAnnotation.coordinate = CLLocationCoordinate2DMake(currentLatitude, currentLongitude)
         }
     }
     
-       
+    
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        var annoView = SpaceStation_AnnotationView()
+        
+        annoView.annotation = annotation
+        
+        return annoView
+        
+    }
     
     
     
